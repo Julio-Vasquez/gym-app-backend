d891431e152ee5ed3d379b0b883b29aa0a9a9a3d
Julio Vasquez <jualvalitube@gmail.com>
2020-10-02 22:10:34 -0500

    feat(sql): notes sql

--- database.sql
+++ database.sql
@@ -1,4 +1,43 @@
 CREATE DATABASE gymdb;
 
-CREATE SCHEMA AUDITS;
-CREATE SCHEMA USERS;
+SET GLOBAL event_scheduler = ON;
+
+CREATE OR REPLACE EVENT check_suscription
+ON SCHEDULE EVERY 1 DAY 
+STARTS CONCAT(CURRENT_DATE(),' ','05:15:00')
+DO 
+UPDATE people set gender = 'Masculino'; 
+
+
+
+
+/*
+
+select DATE_ADD(CURRENT_DATE(),INTERVAL 30 DAY);
+
+select DATE_SUB(CURRENT_DATE(),INTERVAL 10 day);
+
+select DATEDIFF(CURRENT_DATE(), '1996-02-22')
+
+
+/*
+        try {
+          await queryRunner.query(``);
+        } catch (error) {
+          await queryRunner.rollbackTransaction();
+        } finally {
+          await queryRunner.release();
+        }/*
+        try {
+          await getManager().transaction(async manager => {
+    
+            await manager.save(this.suscriptionRepository.create({
+              cost: pay.cost,
+              people: user,
+              days: pay.days,
+              start: `CURRENT_DATE()`,
+              end: `DATE_ADD(CURRENT_DATE(),INTERVAL ${pay.days} DAY)`
+            }))
+          });
+        } catch (error) { return { error: 'TRANSACTION_ERROR', detail: error } }*/
+*/
