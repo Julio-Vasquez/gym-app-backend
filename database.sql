811bf8ab334685b8442b6e9ee1951b588a26d0a5
Julio Vasquez <jualvalitube@gmail.com>
2020-10-19 17:24:37 -0500

    feat(sql): update event

--- database.sql
+++ database.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE gymdb;
 
 SET GLOBAL event_scheduler = ON;
+
 CREATE PROCEDURE procedure_check_suscription()
 BEGIN
 	DECLARE done INT DEFAULT FALSE;
@@ -17,7 +18,9 @@ BEGIN
       LEAVE read_loop;
     END IF;
     IF (DATEDIFF(endtime, CURRENT_DATE()) < 0) THEN
-      UPDATE suscription SET suscription.state = 'inactive' WHERE suscription.id = idsuscription;
+      UPDATE suscription SET suscription.state = 'inactive', suscription.days = 0 WHERE suscription.id = idsuscription;
+    ELSE  
+      UPDATE suscription SET suscription.days = IF(suscription.days = 0, 0,(suscription.days-1)) WHERE suscription.id = idsuscription; 
     END IF;
   END LOOP;
 
@@ -32,37 +35,3 @@ ON SCHEDULE EVERY 1 DAY
 STARTS CONCAT(CURRENT_DATE(),' ','05:15:00')
 DO 
 CALL gymdb.procedure_check_suscription(); 
-
-
-
-
-/*
-
-select DATE_ADD(CURRENT_DATE(),INTERVAL 30 DAY);
-
-select DATE_SUB(CURRENT_DATE(),INTERVAL 10 day);
-
-select DATEDIFF(CURRENT_DATE(), '1996-02-22')
-
-
-/*
-        try {
-          await queryRunner.query(``);
-        } catch (error) {
-          await queryRunner.rollbackTransaction();
-        } finally {
-          await queryRunner.release();
-        }/*
-        try {
-          await getManager().transaction(async manager => {
-    
-            await manager.save(this.suscriptionRepository.create({
-              cost: pay.cost,
-              people: user,
-              days: pay.days,
-              start: `CURRENT_DATE()`,
-              end: `DATE_ADD(CURRENT_DATE(),INTERVAL ${pay.days} DAY)`
-            }))
-          });
-        } catch (error) { return { error: 'TRANSACTION_ERROR', detail: error } }*/
-*/
