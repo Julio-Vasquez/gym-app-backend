f5f05b20bbc2bbdd0d6c46cacf86e78d2290c540
Julio Vasquez <jualvalitube@gmail.com>
2020-10-19 17:37:17 -0500

    feat(database): default user update

--- src/modules/@common/database/user.default.ts
+++ src/modules/@common/database/user.default.ts
@@ -9,15 +9,15 @@ const USERS = [
     username: 'pulido'.toLowerCase(),
     password: '451504911110010510811711296495150491111001051081171124',
     key: '9872c96a-7b13-4bf9-88cd-5965f3df61f7',
-    mail: /*'lucho9515baco@gmail.com'*/'jualvalitube@gmail.com',
+    mail: 'lucho9515baco@gmail.com',
     people: '7c556aca-72ba-4d3d-8f0e-dd57ff0869ef',
   },
   {
     id: 'c66446ad-e580-433a-a7d1-22350a8b57f8',
     username: 'carlos'.toLowerCase(),
-    password: '123123',
+    password: '45150491151111081149799964951504911511110811497994',
     key: 'e5552f6a-bf3b-4205-a0c5-070a99fe8c82',
-    mail:'crojas02@outlook.com',
+    mail: 'crojas02@outlook.com',
     people: 'c1f8dd00-7aad-437e-8029-b44faa06f107',
   },
 ];
