0be6eee9b6ec48fdb29a03857e03c795cb592434
Julio-Vasquez <jualvalitube@gmail.com>
2020-11-04 16:43:04 -0500

    feat(database): update cursor

--- database.sql
+++ database.sql
@@ -9,7 +9,7 @@ BEGIN
 	DECLARE done INT DEFAULT FALSE;
   DECLARE idsuscription VARCHAR(36);
   DECLARE endtime DATE;
-  DECLARE cur1 CURSOR FOR SELECT id, end FROM gymdb.suscription WHERE state = 'active';
+  DECLARE cur1 CURSOR FOR SELECT id, end FROM gymdb.suscription WHERE state = 'active' AND concept = "Mensual";
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
   OPEN cur1;
@@ -31,9 +31,49 @@ END; //
 
 DELIMITER ;
 
+/*Falta modificarlo*/
+DELIMITER //
 
-CREATE OR REPLACE EVENT check_suscription
-ON SCHEDULE EVERY 1 DAY 
-STARTS CONCAT(CURRENT_DATE(),' ','05:15:00')
-DO 
-CALL gymdb.procedure_check_suscription(); 
+CREATE PROCEDURE procedure_check_suscription_tiquetera()
+BEGIN
+	DECLARE done INT DEFAULT FALSE;
+  DECLARE idsuscription VARCHAR(36);
+  DECLARE endtime DATE;
+  DECLARE daysSuscription INT;
+  DECLARE cur1 CURSOR FOR SELECT id, end, days FROM gymdb.suscription WHERE state = 'active' AND concept ='Tiquetera';
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+  OPEN cur1;
+
+  read_loop: LOOP
+    FETCH cur1 INTO idsuscription, endtime, daysSuscription;
+    IF done THEN
+      LEAVE read_loop;
+    END IF;
+    IF (DATEDIFF(endtime, CURRENT_DATE()) < 0 OR daysSuscription = 0) THEN
+      UPDATE 
+        suscription 
+      SET 
+        suscription.state = 'inactive',
+        suscription.days = 0 ,
+        suscription.end = CURRENT_TIMESTAMP()
+      WHERE 
+        suscription.id = idsuscription;
+    END IF;
+  END LOOP;
+
+  CLOSE cur1;
+END; //
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE CHEKEAR()
+BEGIN 
+  CALL procedure_check_suscription();
+  CALL procedure_check_suscription_tiquetera();
+END; $$
+
+DELIMITER ;
