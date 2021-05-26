7bdead52d58a824259f094afa91af5510ba95e3f
KevinGalindo <kevingalindos@gmail.com>
2021-05-26 18:33:59 -0500

    Slint error

--- src/modules/@common/providers/db.provider.ts
+++ src/modules/@common/providers/db.provider.ts
@@ -3,6 +3,7 @@ import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Injectable()
 export class DBProvider implements TypeOrmOptionsFactory {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
